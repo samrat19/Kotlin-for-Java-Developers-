d593d3c248170bb4b8ba88a15b60d352b8afe00a
ATRI DAS <babinsamrat123@gmail.com>
2019-03-28 22:56:51 +0530

    upgraded

--- src/intro to class.kt
+++ src/intro to class.kt
@@ -1,26 +1,70 @@
- in java
+// in java
+//
+//public class Person {
+//
+//    private final String name;
+//    private final int age;
+//
+//    public Person(String name,int age){
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public String getname(){
+//        return name;
+//    }
+//
+//    public int getage(){
+//        return age;
+//    }
+//}
+//
+// in Kotlin
+//
+//class Person(val name:String, val age:Int)
+//
+// Done, all the constructor and the getter all together
 
-public class Person {
+in java
 
-    private final String name;
-    private final int age;
+public void updateweather(int degrees){
 
-    public Person(String name,int age){
-        this.name = name;
-        this.age = age;
-    }
+    String description;
+    Colour colour;
 
-    public String getname(){
-        return name;
-    }
-
-    public int getage(){
-        return age;
+    if(degrees < 5){
+        description = "Cold";
+        colour = BLUE;
+    }else if(degrees < 23){
+        description = "MID";
+        colour = ORANGE;
+    }else{
+        description = "Cold";
+        colour = RED;
     }
 }
 
- in Kotlin
+in kotlin          use pair keyword
 
-class Person(val name:String, val age:Int)
+fun updateweather(degrees : Int){
 
- Done, all the constructor and the getter all together
+    val(description:String , colour:Colour)=
+        if(degrees < 5){
+            Pair("Cold",BLUE)
+        } else if(degrees < 23){
+            Pair("Mid",ORANGE)
+        } else {
+            Pair("HOt",RED)
+        }
+}
+
+transformed using when keyword
+
+fun updateweather(degrees : Int){
+
+    val(description:String , colour:Colour)= when{
+        degrees < 5 -> Pair("Cold",BLUE)
+        degrees < 23 -> Pair("Mid",ORABGE)
+        else -> Pair("Hot",RED)
+    }
+}
