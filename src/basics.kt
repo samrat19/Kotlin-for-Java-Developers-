06fae021f025db3b1d3ffc11246ee15c324c913e
ATRI DAS <babinsamrat123@gmail.com>
2019-03-29 00:01:28 +0530

    updated

--- src/basics.kt
+++ src/basics.kt
@@ -7,13 +7,29 @@ fun foo():String{
     return "foo"
 }
 
-//output
-//
-//calculating to....
-//calculating to....
-//First foo, Second foo
+output
+
+calculating to....
+calculating to....
+First foo, Second foo
 
 kotlin variables
 
         i val comes from value Read-Only correspondes final variable in java
         ii var comes from variable , mutable variable
+
+modification of val
+
+val language = mutableListOf("Java")
+language.add("kotlin")
+
+
+kotlin functions
+
+fun checksmall(a :Int, b:Int):Int{
+    return if (a<b) a else b
+}
+
+transformed to
+
+fun checksmall(a : Int, b : Int):Int = if (a < b) a else b
