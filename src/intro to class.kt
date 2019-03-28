f6a1004be20f9eb3fee77ca5bdc255cd446fec1d
ATRI DAS <babinsamrat123@gmail.com>
2019-03-28 23:00:35 +0530

    Update intro to class.kt

--- src/intro to class.kt
+++ src/intro to class.kt
@@ -1,29 +1,29 @@
-// in java
-//
-//public class Person {
-//
-//    private final String name;
-//    private final int age;
-//
-//    public Person(String name,int age){
-//        this.name = name;
-//        this.age = age;
-//    }
-//
-//    public String getname(){
-//        return name;
-//    }
-//
-//    public int getage(){
-//        return age;
-//    }
-//}
-//
-// in Kotlin
-//
-//class Person(val name:String, val age:Int)
-//
-// Done, all the constructor and the getter all together
+in java
+
+public class Person {
+
+   private final String name;
+   private final int age;
+
+   public Person(String name,int age){
+       this.name = name;
+       this.age = age;
+   }
+
+   public String getname(){
+       return name;
+   }
+
+   public int getage(){
+       return age;
+   }
+}
+
+in Kotlin
+
+class Person(val name:String, val age:Int)
+
+Done, all the constructor and the getter all together
 
 in java
 
